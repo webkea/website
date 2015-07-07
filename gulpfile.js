f2fc858ec130703df227b36c916f250453b477df
Benji Dalton <daylightsavings@gmail.com>
2015-07-08 00:03:00 +1200

    adjusts gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('default', function() {
 
 // Compile slim
 gulp.task('slim', function(){
-  gulp.src("source/*.slim")
+  gulp.src("*.slim")
     .pipe(slim({
       pretty: true
     }))
@@ -26,52 +26,34 @@ gulp.task('slim', function(){
     .pipe(notify('Slim Compiled'));
 });
 
-// Minifiy js
-gulp.task('compress', function() {
-  gulp.src('source/js/*.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('html/js/'))
-});
-
-// Minifiy images
-gulp.task('images', function () {
-  return gulp.src('source/img/*')
-  .pipe(imagemin({
-    progressive: true,
-    svgoPlugins: [{removeViewBox: false}],
-    use: [pngquant()]
-  }))
-  .pipe(gulp.dest('html/img'));
-});
-
 // Static Server + watching scss/html files
 gulp.task('serve', ['scss'], function() {
   browserSync.init({
       server: "html"
   });
-  gulp.watch("source/scss/*.scss", ['scss']);
-  gulp.watch("html/*.html").on('change', browserSync.reload);
+  gulp.watch("css/*.scss", ['scss']);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 // Compile Sass
 gulp.task('scss', function () {
-  gulp.src('source/css/*.scss')
+  gulp.src('css/*.scss')
   .pipe(scss({
     paths: [ path.join(__dirname, 'scss', 'includes') ]
   }))
   .pipe(scss({compress: true}).on('error', gutil.log))
   .pipe(autoprefixer('last 10 versions', 'ie 9'))
   .pipe(minifyCSS({keepBreaks: false}))
-  .pipe(gulp.dest('html/css'))
+  .pipe(gulp.dest('css'))
   .pipe(notify('Sass Compiled, Prefixed and Minified'))
   .pipe(browserSync.stream());
 });
 
 // Watch files
 gulp.task('watch', function () {
-  gulp.watch('source/css/**/*.scss', ['scss']);
-  gulp.watch('source/*.slim', ['slim']);
-  gulp.watch(['html/index.html', 'html/js/**/*', 'source/img/*']);
+  gulp.watch('css/**/*.scss', ['scss']);
+  gulp.watch('*.slim', ['slim']);
+  gulp.watch(['index.html', 'js/**/*', 'img/*']);
 });
 
-gulp.task('default', ['slim', 'images', 'watch', 'compress', 'serve']);
+gulp.task('default', ['slim', 'watch', 'serve']);
