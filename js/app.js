fed3a00642b07e60fd7f44b1aafd7e8c2383e355
Benji Dalton <daylightsavings@gmail.com>
2015-07-14 23:28:00 +1200

    adds custom nav

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
+  $('.menu-button, ul a').click(function(){
+    $('ul').toggleClass('show-menu');
+    $('.menu-button').toggleClass('menu-open');
+    $('.menu-button').toggleClass('menu-close');
+  });
   $(function() {
-      $('.locale-pink').click(function(e) {
+      $('.locale-pink').click(function() {
           $(this).find('iframe').css('pointer-events', 'all');
-      }).mouseleave(function(e) {
+      }).mouseleave(function() {
           $(this).find('iframe').css('pointer-events', 'none');
       });
-  })
+  });
 });
---
+++ js/min/app-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".menu-button, ul a").click(function(){$("ul").toggleClass("show-menu"),$(".menu-button").toggleClass("menu-open"),$(".menu-button").toggleClass("menu-close")}),$(function(){$(".locale-pink").click(function(){$(this).find("iframe").css("pointer-events","all")}).mouseleave(function(){$(this).find("iframe").css("pointer-events","none")})})});
